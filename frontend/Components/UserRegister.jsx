c992793f751a96dd0447a2c9d956753c6b532be1
Geetha Sidhambaram <sidh.geetha@gmail.com>
2024-06-07 20:52:03 +0100

    added link component

--- frontend/Components/UserRegister.jsx
+++ frontend/Components/UserRegister.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
